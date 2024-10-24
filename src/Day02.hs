a45cbb778bda2ce2758afa36a2bca7bb320184ad
flakpm <officialflakpm@gmail.com>
2024-10-24 20:38:32 +0000

    .

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,8 +1,8 @@
 module Day02 where
 
 import Control.Arrow ((&&&))
--- import Control.Monad (ap)
--- import Data.Bool (bool)
+import Control.Monad (ap, liftM2)
+import Data.Bool (bool)
 import Data.Char (isDigit)
 import Paths_aoc2023 (getDataFileName)
 
@@ -19,7 +19,7 @@ splitAllGames :: [String] -> [(String, [String])]
 splitAllGames = map splitSingleGame
 
 splitSingleGame :: String -> (String, [String])
-splitSingleGame = (head &&& splitOn ';' . last) . splitOn ':' . drop 4 . filter (/= ' ')
+splitSingleGame = (head &&& splitOn ';' . last) . splitOn ':' . drop 4 . filter (' ' /=)
 
 validateAllGames :: [String] -> Bool
 validateAllGames = all validateGame
@@ -28,17 +28,18 @@ validateGame :: String -> Bool
 validateGame = validateSet . map (takeWhile isDigit &&& dropWhile isDigit) . splitOn ','
 
 validateSet :: [(String, String)] -> Bool
-validateSet = flip all cubeMap . validateSingleColor
+validateSet = flip all cubeMap . flip validateSingleColor
 
-validateSingleColor :: [(String, String)] -> (String, Int) -> Bool
-validateSingleColor x y = (snd y >=) . sum $ (readStringList (map fst (filter ((fst y ==) . snd) x)))
+validateSingleColor :: (String, Int) -> [(String, String)] -> Bool
+validateSingleColor x = (snd x >=) . sum . readStringList . map fst . filter ((fst x ==) . snd)
 
 readStringList :: [String] -> [Int]
-readStringList xs = if null xs then [0] else map read xs
+readStringList = ap (bool [0] . (map read)) (not . null)
 
 splitOn :: Char -> String -> [String]
-splitOn _ [] = []
-splitOn c s = takeWhile (/= c) s : splitOn c (if null (dropWhile (/= c) s) then [] else tail (dropWhile (/= c) s))
+splitOn = (`ap` (not . null)) . (bool [] .) . (liftM2 (:) (uncurry $ takeWhile . (/=)) (uncurry $ liftM2 (.) splitOn ((ap (bool [] . tail) (not . null) .) . dropWhile . (/=))) .) . (,)
+
+
 
 cubeMap :: [(String, Int)]
 cubeMap = [("red", 12), ("green", 13), ("blue", 14)]
